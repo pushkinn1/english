aef2c5933c08d33085016b5d761a51185329a662
Kirill <k1rilllavrinenko@yandex.ru>
2021-10-03 17:26:29 +0300

    Подготовка к слиянию

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React from "react";
-import server from "./api"
+import ThemeSelection from "./components/themeselection";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
+import './header.css'
 
 class App extends React.Component {
-    componentDidMount() {
-        fetch("api/questions")
-            .then(r => r.json())
-            .then(r => console.log(r["questions"]))
-    }
     render() {
         return (
-            <div>Hello test</div>
+            <Router>
+                <header className="header">
+                    <div className="wrapper header__wrapper">
+                        <div className="header__logo">
+                            <Link to="/">Logo</Link>
+                        </div>
+                        <div className="header__auth">
+                            <Link to="/signUp" className="btn auth__btn">
+                                <span>Sign up</span>
+                            </Link>
+                            <Link to="/signIn" className="btn auth__btn">
+                                <span>Sign in</span>
+                            </Link>
+                        </div>
+                    </div>
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <Main />
+                        </Route>
+                        <Route path="/themeSelection">
+                            <ThemeSelection />
+                        </Route>
+                    </Switch>
+                </main>
+                <footer className="footer">
+
+                </footer>
+            </Router>
         )
     }
+}
+
+function Main() {
+    return (
+        <div>Welcome to our app <Link to="/themeSelection">select theme</Link></div>
+    )
 }
 
 export default App
